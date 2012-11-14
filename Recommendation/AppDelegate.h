f36d2118d159e02b3c50206ea467507deafb5a9b
ilker <ilker@ilker-macbook.local>
2012-11-14 22:51:05 +0100

    Initial Commit

---
+++ Recommendation/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Recommendation
+//
+//  Created by ilker on 14.11.12.
+//  Copyright (c) 2012 ilker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Recommendation/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Recommendation
+//
+//  Created by ilker on 14.11.12.
+//  Copyright (c) 2012 ilker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
